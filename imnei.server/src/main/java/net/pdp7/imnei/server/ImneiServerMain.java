03115a63921e72994e5bb04f2245fbdc3f679523
alex <alex@pdp7.net>
2018-06-09 13:39:21 +0200

    Remove Kafka (to be moved to a branch)

--- imnei.server/src/main/java/net/pdp7/imnei/server/ImneiServerMain.java
+++ imnei.server/src/main/java/net/pdp7/imnei/server/ImneiServerMain.java
@@ -2,7 +2,6 @@ package net.pdp7.imnei.server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.GenericContainer;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -12,13 +11,10 @@ public class ImneiServerMain {
 	Logger logger = LoggerFactory.getLogger(getClass());
 
 	public void start() throws Exception {
-		try (GenericContainer<?> kafkaContainer = new GenericContainer<>("spotify/kafka:latest")) {
-			kafkaContainer.start();
-			Server server = ServerBuilder.forPort(0).addService(new ImneiProtoImpl(new ImneiServer())).build();
-			server.start();
-			logger.info("running on port {}", server.getPort());
-			server.awaitTermination();
-		}
+		Server server = ServerBuilder.forPort(0).addService(new ImneiProtoImpl(new ImneiServer())).build();
+		server.start();
+		logger.info("running on port {}", server.getPort());
+		server.awaitTermination();
 	}
 
 	public static void main(String[] args) throws Exception {
