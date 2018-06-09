760b2e070527211e69fc772cd501e57d7e6d18dd
alex <alex@pdp7.net>
2018-06-09 13:03:08 +0200

    Move code to a class

--- imnei.pyclient/imnei.py
+++ imnei.pyclient/imnei.py
@@ -10,21 +10,36 @@ from imnei_grpc import imnei_pb2_grpc
 def connect_request(connection_id):
     return imnei_pb2.ChatRequest(connectRequest=imnei_pb2.ConnectRequest(connectionId=connection_id))
 
-def main():
-    channel = grpc.insecure_channel(sys.argv[1])
-    stub = imnei_pb2_grpc.ImneiStub(channel)
-    
-    connection_id = sys.argv[2] if len(sys.argv) == 3 else None
-    
-    send_queue = queue.Queue()
-    send_queue.put(connect_request(connection_id))
-    
-    def send_queue_iter():
+
+class ImneiClient:
+    def __init__(self, server, connection_id):
+        self.server = server
+        self.connection_id = connection_id
+        self.send_queue = queue.Queue()
+
+    def start(self):
+        self._connect_to_server()
+        self._send_connect_request()
+        for received in self.stub.Chat(self._send_queue_iter()):
+            print(received)
+
+    def _connect_to_server(self):
+        self.channel = grpc.insecure_channel(self.server)
+        self.stub = imnei_pb2_grpc.ImneiStub(self.channel)
+
+    def _send_connect_request(self):
+        self.send_queue.put(connect_request(self.connection_id))
+
+    def _send_queue_iter(self):
         while True:
-            yield send_queue.get(True)
-    
-    for received in stub.Chat(send_queue_iter()):
-        print(received)
+            yield self.send_queue.get(True)
+
+
+def main():
+    server = sys.argv[1]
+    connection_id = sys.argv[2] if len(sys.argv) == 3 else None
+    client = ImneiClient(server, connection_id)
+    client.start()
 
 
 if __name__ == '__main__':
