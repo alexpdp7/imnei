32481ea614d4a6498318d1aca35c5b693c69f097
alex <alex@pdp7.net>
2018-06-09 12:09:38 +0200

    Add Kafka container

--- imnei.server/src/main/java/net/pdp7/imnei/server/ImneiServerMain.java
+++ imnei.server/src/main/java/net/pdp7/imnei/server/ImneiServerMain.java
@@ -1,14 +1,19 @@
 package net.pdp7.imnei.server;
 
+import org.testcontainers.containers.GenericContainer;
+
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import net.pdp7.imnei.server.grpc.impl.ImneiProtoImpl;
 
 public class ImneiServerMain {
 	public static void main(String[] args) throws Exception {
-		Server server = ServerBuilder.forPort(0).addService(new ImneiProtoImpl(new ImneiServer())).build();
-		server.start();
-		System.out.println(server.getPort());
-		server.awaitTermination();
+		try(GenericContainer<?> kafkaContainer = new GenericContainer<>("spotify/kafka:latest")) {
+			kafkaContainer.start();
+			Server server = ServerBuilder.forPort(0).addService(new ImneiProtoImpl(new ImneiServer())).build();
+			server.start();
+			System.out.println(server.getPort());
+			server.awaitTermination();
+		}
 	}
 }
