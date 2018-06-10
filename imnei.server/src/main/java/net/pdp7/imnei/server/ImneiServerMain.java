853e2130de2c2dcf190606a522695b51efda3acb
alex <alex@pdp7.net>
2018-06-10 12:57:48 +0200

    Basic imnei.webclient
    * Add testcontainer grpcwebproxy which uses unmerged PR websockets
    * Make server ports customizable
    * Add sample TLS keys and document how to generate new ones
    * Rip off Typescript project setup from
    
    https://github.com/improbable-eng/grpc-web/pull/180/files
    
    * Add initial communication code ripped off from:
    
    https://github.com/improbable-eng/grpc-web/blob/master/test/ts/src/client.websocket.spec.ts#L77

--- imnei.server/src/main/java/net/pdp7/imnei/server/ImneiServerMain.java
+++ imnei.server/src/main/java/net/pdp7/imnei/server/ImneiServerMain.java
@@ -2,6 +2,10 @@ package net.pdp7.imnei.server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+import org.testcontainers.containers.SelinuxContext;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -9,15 +13,42 @@ import net.pdp7.imnei.server.grpc.impl.ImneiProtoImpl;
 
 public class ImneiServerMain {
 	Logger logger = LoggerFactory.getLogger(getClass());
+	protected final int grpcPort;
+	protected final int grpcWebPort;
+
+	public ImneiServerMain(int grpcPort, int grpcWebPort) {
+		this.grpcPort = grpcPort;
+		this.grpcWebPort = grpcWebPort;
+	}
 
 	public void start() throws Exception {
-		Server server = ServerBuilder.forPort(0).addService(new ImneiProtoImpl(new ImneiServer())).build();
+		Server server = ServerBuilder.forPort(grpcPort).addService(new ImneiProtoImpl(new ImneiServer())).build();
 		server.start();
-		logger.info("running on port {}", server.getPort());
-		server.awaitTermination();
+		String serverEndPoint = "localhost:" + server.getPort();
+		try (FixedHostPortGenericContainer<?> grpcWebProxy = new FixedHostPortGenericContainer<>("alexpdp7/grpcwebproxyws:latest")) {
+			grpcWebProxy
+				.withCommand(
+						"/go/src/github.com/improbable-eng/grpc-web/go/grpcwebproxy/grpcwebproxy",
+						"--backend_addr=" + serverEndPoint,
+						"--server_http_tls_port=" + grpcWebPort,
+						"--server_tls_cert_file=" + "/public.key",
+						"--server_tls_key_file=" + "/private.key",
+						"--backend_tls_noverify",
+						"--use_websockets")
+				.withNetworkMode("host")
+				.withClasspathResourceMapping("/public.key", "/public.key", BindMode.READ_ONLY, SelinuxContext.SHARED)
+				.withClasspathResourceMapping("/private.key", "/private.key", BindMode.READ_ONLY, SelinuxContext.SHARED)
+				.start();
+			grpcWebProxy.followOutput(new Slf4jLogConsumer(LoggerFactory.getLogger("net.pdp7.imnei.server.grpcwebproxy")));
+			String grpcWebEndpoint = "localhost:" + grpcWebPort;
+			logger.info("GRPC on {}, GRPCWEB on {}", serverEndPoint, grpcWebEndpoint);
+			server.awaitTermination();
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		new ImneiServerMain().start();
+		int grpcPort = Integer.parseInt(args[0]);
+		int grpcWebPort = Integer.parseInt(args[1]);
+		new ImneiServerMain(grpcPort, grpcWebPort).start();
 	}
 }
---
+++ imnei.webclient/protogen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ./ts/_proto
+
+echo "Compiling protobuf definitions"
+protoc \
+  --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+  -I ./proto \
+  --js_out=import_style=commonjs,binary:./ts/_proto \
+  --ts_out=service=true:./ts/_proto \
+  ./proto/imnei.proto
