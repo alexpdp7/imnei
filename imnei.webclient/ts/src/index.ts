010a195783df6f359c40ec2195b6c9f3d773ff95
alex <alex@pdp7.net>
2018-06-17 17:53:48 +0200

    Crummy subscribe implementation
    * Refactor out createChatElement

--- imnei.webclient/ts/src/index.ts
+++ imnei.webclient/ts/src/index.ts
@@ -12,10 +12,39 @@ let imneiClient = grpc.client(Imnei.Chat, {
 
 let connectionId = null;
 
+function createChatElement(chatId: string) {
+  let chatHeader = `
+    <h2 id="header_${chatId}">${chatId}</h2>
+    <ul></ul>
+    <input type="text"/> <button>Send</button>
+  `;
+  document.getElementById("chats")!.innerHTML += chatHeader;
+  let chatInput = <HTMLInputElement>document.querySelector(`#header_${chatId} ~ input`)!;
+  document.querySelector(`#header_${chatId} ~ button`)!.addEventListener("click", () => {
+  let chatRequest = new ChatRequest();
+  let message = new Message();
+  message.setChatid(chatId);
+  message.setMessage(chatInput.value);
+  chatRequest.setMessage(message);
+  console.log("send", chatRequest);
+  imneiClient.send(chatRequest);
+  });
+}
+
 document.getElementById("new_chat")!.addEventListener("click",() => {
   let chatRequest = new ChatRequest()
   chatRequest.setNewchatrequest(new NewChatRequest());
   imneiClient.send(chatRequest);
+});
+
+document.getElementById("subscribe")!.addEventListener("click",() => {
+  let chatRequest = new ChatRequest();
+  let subscribeRequest = new SubscribeChatRequest();
+  let subscribeId = <HTMLInputElement>document.getElementById("subscribe_id")!;
+  subscribeRequest.setChatid(subscribeId.value);
+  chatRequest.setSubscribechatrequest(subscribeRequest);
+  imneiClient.send(chatRequest);
+  createChatElement(subscribeId.value);
 });
 
 imneiClient.start();
@@ -29,27 +58,12 @@ imneiClient.onMessage((message: ChatResponse) => {
   if(message.hasNewchatresponse()) {
     let chatId = message.getNewchatresponse()!.getChatid();
     console.log("New chat", chatId);
-    let chatHeader = `
-      <h2 id="header_${chatId}">${chatId}</h2>
-      <ul></ul>
-      <input type="text"/> <button>Send</button>
-    `;
-    document.getElementById("chats")!.innerHTML += chatHeader;
-    let chatInput = <HTMLInputElement>document.querySelector(`#header_${chatId} ~ input`)!;
-    document.querySelector(`#header_${chatId} ~ button`)!.addEventListener("click", () => {
-      let chatRequest = new ChatRequest();
-      let message = new Message();
-      message.setChatid(chatId);
-      message.setMessage(chatInput.value);
-      chatRequest.setMessage(message);
-      console.log("send", chatRequest);
-      imneiClient.send(chatRequest);
-    });
     let chatRequest = new ChatRequest();
     let subscribeChatRequest = new SubscribeChatRequest();
     subscribeChatRequest.setChatid(chatId);
     chatRequest.setSubscribechatrequest(subscribeChatRequest);
     imneiClient.send(chatRequest);
+    createChatElement(chatId);
   }
   if(message.hasMessage()) {
     let chatMessage = message.getMessage()!;
